ff303dc953ac4f4e4db9b3ba09c2be83c6b4d9c1
Athanasios Topaltsis <a.topaltsis@gmail.com>
2024-04-19 14:35:43 -0400

    Working on form submit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { FormEvent } from "react";
 import "./css/App.css";
 
 function App() {
@@ -8,15 +9,36 @@ function App() {
         <h2 className="subtitle">Shorten any url you want!</h2>
       </div>
 
-      <form action="" className="shortener-form">
+      <form action="" className="shortener-form" onSubmit={handleFormSubmit}>
         <label htmlFor="url-input" className="label">
           Long URL
         </label>
-        <input type="text" id="url-input" className="input" />
-        <button className="button is-primary">Shorten</button>
+        <input
+          type="text"
+          id="url-input"
+          className="input"
+          placeholder="Enter the url you want to shorten."
+        />
+        <button className="button is-primary" type="submit">
+          Shorten
+        </button>
       </form>
     </div>
   );
+
+  function handleFormSubmit(event: FormEvent): void {
+    // Prevent the form from refreshing the page on submit.
+    event.preventDefault();
+
+    const inputField = document.getElementById("url-input") as HTMLInputElement;
+
+    // if the input field is empty return.
+    if (inputField.value.trim().length == 0) {
+      return;
+    }
+
+    // TODO: submit url to backend (would i validate url here or in the backend?)
+  }
 }
 
 export default App;
