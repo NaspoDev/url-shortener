96844131aff3d487983810990347f5e2f9e81da2
Athanasios Topaltsis <a.topaltsis@gmail.com>
2024-04-19 11:13:13 -0400

    Initial project structure

---
+++ README.md
@@ -0,0 +1,16 @@
+# URL Shortener
+
+A URL Shortener webapp made with React and Spring Boot.
+
+## How it Works
+
+Simply paste in a URL that you want to shorten, and press "shorten".
+
+The app will return a shortened URL that will redirect to the original URL.
+
+## Tech Stack
+
+- React (with TypeScript)
+- Spring Boot (Java)
+- MySQL
+- Served with Docker
---
+++ server/url-shortener-server/src/main/java/dev/naspo/urlshortenerserver/UrlShortenerServerApplication.java
@@ -0,0 +1,13 @@
+package dev.naspo.urlshortenerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortenerServerApplication.class, args);
+	}
+
+}
