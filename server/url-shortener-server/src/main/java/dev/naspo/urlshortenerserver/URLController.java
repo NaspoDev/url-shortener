e90f96f5c523fc11984bf486e1c043c145074e76
Athanasios Topaltsis <a.topaltsis@gmail.com>
2024-04-20 16:45:06 -0400

    Playing around with routing

--- server/url-shortener-server/src/main/java/dev/naspo/urlshortenerserver/URLController.java
+++ server/url-shortener-server/src/main/java/dev/naspo/urlshortenerserver/URLController.java
@@ -1,8 +1,17 @@
 package dev.naspo.urlshortenerserver;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// Handles requests to /urls
+// Handles requests to "/urls"
 @RestController
+@RequestMapping("/urls")
 public class URLController {
+
+    // Testing routing
+    @GetMapping("")
+    public String getHelloWorld() {
+        return "Hello World!";
+    }
 }
